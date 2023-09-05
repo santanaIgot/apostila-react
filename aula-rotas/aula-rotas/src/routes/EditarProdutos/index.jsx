f9002424cd8b111f9c83347595d5bd95d6876032
santanaIgot <santanaigoor01@gmail.com>
2023-09-05 10:10:23 -0300

    Rota Editar Produtos

---
+++ aula-rotas/aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function EditarProdutos() {
+    
+    return(
+        <>
+        <main>
+            <h1>Editar produtos</h1>
+        </main>
+        </>
+    )
+
+}
