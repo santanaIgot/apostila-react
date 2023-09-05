1d07c7bce48a32ffa91915af42ba0775a0eaa60d
santanaIgot <santanaigoor01@gmail.com>
2023-09-05 10:11:24 -0300

    Rota error

---
+++ aula-rotas/aula-rotas/src/routes/Error/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function Error() {
+    
+    return(
+        <>
+        <main>
+            <h1>Page not found</h1>
+        </main>
+        </>
+    )
+
+}
